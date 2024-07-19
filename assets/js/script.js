11d0a00c26a27596aca19932b1d0c84e182f1f9c
LfayeJ <lfaye1213@gmail.com>
2024-07-19 12:42:16 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,33 @@
-// script.js
+// assets/js/script.js
+let posts = [];
+
+// JSON 파일을 로드하여 posts 배열에 저장
+fetch('{{ site.baseurl }}/assets/posts.json')
+    .then(response => response.json())
+    .then(data => {
+        posts = data;
+    })
+    .catch(error => console.error('Error loading posts:', error));
+
+// 검색 폼 제출 이벤트 핸들러
 document.getElementById('search_form').addEventListener('submit', function(event) {
     event.preventDefault(); // 기본 제출 동작을 막음
     const query = document.getElementById('searchinput').value.toLowerCase();
     const resultsList = document.getElementById('results_list');
     resultsList.innerHTML = ''; // 기존 결과를 지움
-    const contentElements = document.querySelectorAll('.primary-content *');
-    let hasResults = false;
 
-    // 기존 하이라이트 제거
-    contentElements.forEach(element => {
-        element.classList.remove('highlight');
-    });
+    const results = posts.filter(post => post.content.toLowerCase().includes(query));
 
-    // 검색어를 포함하는 요소 찾기
-    contentElements.forEach(element => {
-        if (element.innerText.toLowerCase().includes(query)) {
-            hasResults = true;
-            element.classList.add('highlight'); // 요소에 하이라이트 클래스 추가
+    if (results.length > 0) {
+        results.forEach(result => {
             const li = document.createElement('li');
             const a = document.createElement('a');
-            a.href = `#`;
-            a.textContent = `Result for "${query}": ${element.innerText.substring(0, 30)}...`;
-            a.addEventListener('click', () => {
-                element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-            });
+            a.href = result.url;
+            a.textContent = `Result for "${query}": ${result.title}`;
             li.appendChild(a);
             resultsList.appendChild(li);
-        }
-    });
-
-    if (!hasResults) {
+        });
+    } else {
         const li = document.createElement('li');
         li.textContent = 'No results found';
         resultsList.appendChild(li);
